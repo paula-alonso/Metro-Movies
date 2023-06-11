58d2b6eba857c392dc1632822bafc82f0f8a84c3
Paula Alonso <paula.alonso@correo.unimet.edu.ve>
2023-06-11 15:31:02 -0400

    Funciona el registro

--- src/Pages/SignupPage/SignupPage.jsx
+++ src/Pages/SignupPage/SignupPage.jsx
@@ -1,45 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from '../LoginPage/LoginPage.module.css'
-import { singInWithGoogle } from '../../firebase/auth-service';
+import { registerEmail, singInWithGoogle } from '../../firebase/auth-service';
 
 export function SignupPage() {
 
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
     const handleSingWithGoogle = async () => {
         await singInWithGoogle()
+        navigate("/");
     };
+
+    const handleOnChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    const submit=async (event) => {
+        event.preventDefault();
+        const {email, password, ...extraData} = formData
+        await registerEmail(email, password, extraData);
+        navigate("/");
+    }
 
   return (
     <div className={styles.container}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={submit}>
             <img src="src/assets/Logo/logo.png"/>
             <h1 className={styles.title}>Bienvenido a Metro Movies</h1>
             <p className={styles.subtitle}>
             ¡Bienvenido! introduce tus datos para registrarte
             </p>
 
+            {/* NAME FIELD */}
+            <div className={styles.inputContainer}>
+                <label htmlFor="name">
+                    <span>Nombre</span>
+                </label>
+                <input type="name" name="name" id="name" placeholder="Ej. Juan Pérez" onChange={handleOnChange}/>
+            </div>
+
+        
+
             {/* EMAIL FIELD */}
             <div className={styles.inputContainer}>
                 <label htmlFor="email">
                     <span>Correo electrónico</span>
                 </label>
-                <input type="email" name="email" id="email" placeholder="Ej. juanperez@gmail.com"/>
+                <input type="email" name="email" id="email" placeholder="Ej. juanperez@gmail.com" onChange={handleOnChange}/>
             </div>
 
-        
-
             {/* PASSWORD FIELD */}
+
             <div className={styles.inputContainer}>
                 <label htmlFor="password">
                     <span>Contraseña</span>
                 </label>
-                <input type="password" name="password" id="password" placeholder="*********"/>
-            </div>
-            <div className={styles.inputContainer}>
-                <label htmlFor="confirm_password">
-                    <span>Confirmar contraseña</span>
-                </label>
-                <input type="confirm_password" name="password" id="confirm_password" placeholder="*********"/>
+                <input type="password" name="password" id="password" placeholder="*********" onChange={handleOnChange}/>
             </div>
 
             {/* BUTTONS */}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,70 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
+import { useUser } from '../../contexts/UserContext'
+import { logOut } from '../../firebase/auth-service';
 
 export function Navbar() {
 
+    const {user} = useUser();
+    console.log(user)
+
+    const handleLogout = async () => {
+        console.log("saliendo...")
+        await logOut();
+    }
+
     return (
-        <div className={styles.navbar}>
+        <nav className={styles.navbar}>
             <div className={styles.navdiv}>
             <div className={styles.logo}><img src='src/assets/Logo/logo.png' className={styles.logo}></img></div>
             
-                <ul>
+            <ul className={styles.infoContainer}>
                 <li>
                     <Link to={'/'} className={styles.link}>
                         <a>Home</a>
                     </Link>
                 </li>
-                <li>
-                    <Link to={'/reservar'} className={styles.link}>
-                        <a>Reservar</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/login'} className={styles.link}>
-                        <button className={styles.buttonInicio}>Iniciar</button>
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/signup'} className={styles.link}>
-                        <button className={styles.buttonInicio}>Registrarse</button>
-                    </Link>
-                </li>
+                {!!user && (
+                    <>
+                        <li>
+                        <h1 className={styles.profile}>
+                            <a>Hola, {user.name}</a>
+                        </h1>
+                        </li>
+                        <li>
+                            <Link to={'/profile'} className={styles.link}>
+                                <a>Perfil</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <button className={styles.buttonInicio} onClick={handleLogout}>Salir</button>
+                        </li>
+                    </>
+                )
+
+                }
+
+                {!user &&(
+                <>
+                        <li>
+                        <Link to={'/login'} className={styles.link}>
+                            <button className={styles.buttonInicio}>Iniciar</button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/signup'} className={styles.link}>
+                            <button className={styles.buttonInicio}>Registrarse</button>
+                        </Link>
+                    </li>
+                </>
+                )}
 
             </ul>
 
             </div>
 
-        </div>
+        </nav>
 
     )
 }
